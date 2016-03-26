ee22344efb7333b060b47319ec9312be12480676
Ben Pang <benjinpang@gmail.com>
2016-03-26 10:09:58 -0700

    add more functions

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ stack=["0"]
 base=36
 p=function(x){return BigInteger.parse(x,base)};
 commands={
+	"ǁ":function(){if(stack.length)stack.push(stack[stack.length-1])},
+	"ǂ":function(){if(stack.length)stack.pop()},
+	"¤":function(){if(stack.length>1)stack.push(stack[stack.length-2])},
+	"ç":function(){stack[stack.length-1]=stack[stack.length-1].split(''),stack[stack.length-1].pop(),stack[stack.length-1].join('').length?(stack[stack.length-1]=stack[stack.length-1].join('')):stack.pop()},
 	"©":function(){stack=["0"]},
 	"n":function(){stack.push("0")},
 	"+":function(){if(stack.length>1)stack.push(p(stack.pop()).add(p(stack.pop())).toString(base))},
@@ -9,14 +13,14 @@ commands={
 	"×":function(){if(stack.length>1)stack.push(p(stack.pop()).add(p(stack.pop())).toString(base))},
 	"÷":function(){if(stack.length>1)x=stack.pop(),y=stack.pop(),stack.push(p(y).quotient(p(x)).toString(base))},
 	"%":function(){if(stack.length>1)x=stack.pop(),y=stack.pop(),stack.push(p(y).remainder(p(x)).toString(base))},
-	"±":function(){stack.push(p(stack.pop()).negate().toString(base))},
+	"±":function(){if(stack.length)stack.push(p(stack.pop()).negate().toString(base))},
 	"&":function(){if(stack.length>1)stack.push(BigInteger(p(stack.pop()).toString()&p(stack.pop()).toString()).toString(base))},
 	"|":function(){if(stack.length>1)stack.push(BigInteger(p(stack.pop()).toString()|p(stack.pop()).toString()).toString(base))},
 	"^":function(){if(stack.length>1)stack.push(BigInteger(p(stack.pop()).toString()^p(stack.pop()).toString()).toString(base))},
-	"~":function(){stack.push(p(stack.pop()).negate().prev().toString(base))},
+	"~":function(){if(stack.length)stack.push(p(stack.pop()).negate().prev().toString(base))},
 	"«":function(){if(stack.length>1)x=stack.pop(),y=stack.pop(),stack.push((eval('p(y)'+'.multiply(2)'.repeat(p(x)))).toString(base))},
 	"»":function(){if(stack.length>1)x=stack.pop(),y=stack.pop(),stack.push((eval('p(y)'+'.quotient(2)'.repeat(p(x)))).toString(base))},
-	"ß":function(){ob=base,base=p(stack.pop()).toString(),stack=stack.map(function(x){return BigInteger.parse(x,ob).toString(base)})},
+	"ß":function(){if(stack.length)ob=base,base=p(stack.pop()).toString(),stack=stack.map(function(x){return BigInteger.parse(x,ob).toString(base)})},
 	"¹":function(){if(stack.length>1)x=stack.pop(),y=stack.pop(),stack.push(p(y).pow(p(x)).toString(base))},
 	"$":function(){if(stack.length>1)stack.push(stack[stack.length-stack.pop()-2])},
 	"ø":function(){if(stack.length>1)stack.splice(stack.length-stack.pop()-2,1)},
