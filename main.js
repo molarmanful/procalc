671b2a8fc98e9ed5d0c88cf910d35abb36bbc062
Ben Pang <benjinpang@gmail.com>
2016-03-26 10:37:20 -0700

    more symbol changes

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ stack=["0"]
 base=36
 p=function(x){return BigInteger.parse(x,base)};
 commands={
-	"ǁ":function(){if(stack.length)stack.push(stack[stack.length-1])},
-	"ǂ":function(){if(stack.length)stack.pop()},
+	"$":function(){if(stack.length)stack.push(stack[stack.length-1])},
+	"⍖":function(){if(stack.length)stack.pop()},
 	"¤":function(){if(stack.length>1)stack.push(stack[stack.length-2])},
-	"ç":function(){stack[stack.length-1]=stack[stack.length-1].split(''),stack[stack.length-1].pop(),stack[stack.length-1].join('').length?(stack[stack.length-1]=stack[stack.length-1].join('')):stack.pop()},
-	"©":function(){stack=["0"]},
+	"⎌":function(){stack[stack.length-1]=stack[stack.length-1].split(''),stack[stack.length-1].pop(),stack[stack.length-1].join('').length?(stack[stack.length-1]=stack[stack.length-1].join('')):stack.pop()},
+	"⎚":function(){stack=["0"]},
 	"n":function(){stack.push("0")},
 	"+":function(){if(stack.length>1)stack.push(p(stack.pop()).add(p(stack.pop())).toString(base))},
 	"-":function(){if(stack.length>1)x=stack.pop(),y=stack.pop(),stack.push(p(y).subtract(p(x)).toString(base))},
@@ -22,8 +22,8 @@ commands={
 	"»":function(){if(stack.length>1)x=stack.pop(),y=stack.pop(),stack.push((eval('p(y)'+'.quotient(2)'.repeat(p(x)))).toString(base))},
 	"ß":function(){if(stack.length)ob=base,base=p(stack.pop()).toString(),stack=stack.map(function(x){return BigInteger.parse(x,ob).toString(base)})},
 	"¹":function(){if(stack.length>1)x=stack.pop(),y=stack.pop(),stack.push(p(y).pow(p(x)).toString(base))},
-	"$":function(){if(stack.length>1)stack.push(stack[stack.length-stack.pop()-2])},
-	"ø":function(){if(stack.length>1)stack.splice(stack.length-stack.pop()-2,1)},
+	"©":function(){if(stack.length>1)stack.push(stack[stack.length-stack.pop()-2])},
+	"⍗":function(){if(stack.length>1)stack.splice(stack.length-stack.pop()-2,1)},
 	"@":function(){if(stack.length>1)stack.push(stack.splice(stack.length-stack.pop()-2,1))},
 	"ª":function(){if(stack.length>1)stack.splice(stack.length-stack.pop()-2,0,stack.pop())},
 }
