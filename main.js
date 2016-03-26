fbe0bddb187ce518215ad97780d5a17f9c690604
Ben Pang <benjinpang@gmail.com>
2016-03-26 02:24:11 -0700

    better shifts

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-stack=[""]
+stack=["0"]
 base=36
 p=function(x){return BigInteger.parse(x,base)};
 commands={
-	"©":function(){stack=[""]},
-	"n":function(){stack.push("")},
+	"©":function(){stack=["0"]},
+	"n":function(){stack.push("0")},
 	"+":function(){stack.push(p(stack.pop()).add(p(stack.pop())).toString(base))},
 	"-":function(){x=stack.pop(),y=stack.pop(),stack.push(p(y).subtract(p(x)).toString(base))},
 	"×":function(){stack.push(p(stack.pop()).add(p(stack.pop())).toString(base))},
@@ -14,8 +14,8 @@ commands={
 	"|":function(){stack.push(BigInteger(p(stack.pop()).toString()|p(stack.pop()).toString()).toString(base))},
 	"^":function(){stack.push(BigInteger(p(stack.pop()).toString()^p(stack.pop()).toString()).toString(base))},
 	"~":function(){stack.push(p(stack.pop()).negate().prev())},
-	"«":function(){x=stack.pop(),y=stack.pop(),stack.push((p(y)<<p(x)).toString(base))},
-	"»":function(){x=stack.pop(),y=stack.pop(),stack.push((p(y)>>p(x)).pow(p(x)).toString(base))},
+	"«":function(){x=stack.pop(),y=stack.pop(),stack.push((eval('p(y)'+'.multiply(2)'.repeat(p(x)))).toString(base))},
+	"»":function(){x=stack.pop(),y=stack.pop(),stack.push((eval('p(y)'+'.quotient(2)'.repeat(p(x)))).toString(base))},
 	"ß":function(){ob=base,base=p(stack.pop()).toString(),stack=stack.map(function(x){return BigInteger.parse(x,ob).toString(base)})},
 	"¹":function(){x=stack.pop(),y=stack.pop(),stack.push(p(y).pow(p(x)).toString(base))},
 	"$":function(){stack.push(stack[stack.length-stack.pop()-2])},
@@ -24,6 +24,6 @@ commands={
 	"ª":function(){stack.splice(stack.length-stack.pop()-2,0,stack.pop())},
 }
 $('.col').click(function(){
-	$(this).text().match(/[A-Z0-9.]/)?stack[stack.length-1]+=$(this).text():commands[$(this).text()]()
-	$('.disp').html(stack.map(function(x){return x===""?0:x}).reverse().join(' '))
+	$(this).text().match(/[A-Z0-9.]/)?stack[stack.length-1]=stack[stack.length-1]=='0'?stack[stack.length-1]+$(this).text():$(this).text():commands[$(this).text()]()
+	$('.disp').html(stack.slice(0).reverse().join(' '))
 })
